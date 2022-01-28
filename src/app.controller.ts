95219a85413d8e4354d388d3c2ff2a20d230bc82
PearEasy <peareasy@life.com>
2022-01-28 13:01:45 +0000

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post('login')
+  login(@Body() body) {
+    return this.appService.login(body);
+  }
+  @Get()
+  getHello() {
+    return this.appService.getHello();
+  }
+  @Get('empty-ms-endpoint')
+  newEndpoint() {
+    return this.appService.emptyEndpoint();
+  }
+}
